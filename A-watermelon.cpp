c068a4161110746043571e0d93dd756a1198ad5c
Mateo Ibanez <mateoiba30@gmail.com>
2025-04-10 21:06:06 -0300

    agrego archivos

---
+++ A-watermelon.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int peso;
+    cin >> peso;
+
+    if(peso % 2 == 0 and peso !=2){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+
+    return 0;
+}
+
+/*
+muy importante no mostrar nada extra en la pantalla, solo lo que me piden
+*/
---
+++ plantilla.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n; //2^31-1 ~= 10^9 
+    long long x; //2^63-1 8*10^18
+    cin >> n;
+    cout << n*n << "\n";
+
+    return 0;
+}
